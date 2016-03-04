70f843944fff8196c1bc25a6261015b1e5820ec1
Enclave88 <om_red@list.ru>
2016-03-05 02:03:21 +0600

    Start page project

---
+++ app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('modalTest',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+  .controller('dialogTestCtrl',function($scope, $interval){
+
+    $interval(function () {
+      $scope.now = new Date().toLocaleString('ru-RU');
+    });
+
+    $scope.myDate = new Date();
+
+    $scope.chooseYourDestiny = function () {
+      debugger;
+      var countDays = Math.floor(new Date() / 1000 / 60 / 60 / 24 / 365.25);
+      var countDays2 = Math.floor(this.myDate / 1000 / 60 / 60 / 24 / 365.25);
+      var da = countDays - countDays2;
+      if (da > 2) {
+        alert('Enough!');
+      }
+    }
+  });
