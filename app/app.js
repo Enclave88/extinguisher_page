bc83810caaf4ea5d6f845da41799def54dba05d9
Enclave88 <om_red@list.ru>
2016-03-07 23:54:09 +0600

    Something changes and a new icon

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
-'use strict';
+;'use strict';
 
 angular.module('modalTest', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-  .controller('dialogTestCtrl',function($scope, $interval){
+  .controller('dialogTestCtrl', function($scope, $interval){
+
+    $scope.extinguisher = {
+      type: ['powder', 'carbon_dioxide'],
+      weight: true
+    };
 
     $interval(function () {
       $scope.now = new Date().toLocaleString('ru-RU');
@@ -14,10 +19,12 @@ angular.module('modalTest', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.sv
       var dayZero = Math.round(new Date() / 1000 / 60 / 60 / 24 / 365.25);
       var beginCount = Math.round(this.myDate / 1000 / 60 / 60 / 24 / 365.25);
       var summary = (dayZero - beginCount);
-      if (summary >= 2) {
+      if (summary > 2) {
         alert('Пора перезаряжать!');
-      } else if (summary >= 1) {
+      } else if (summary >= 1 && this.extinguisher.type === 'powder') {
           alert('Время освидетельствования');
+      } else if (summary >= 1 ) {
+          alert('Автомобильный');
       } else if (summary < 1) {
           alert('Всё в порядке');
       }
