26455d87fc087dee9e1086088321552865b23804
Enclave88 <om_red@list.ru>
2016-03-10 23:48:33 +0600

    nothing

--- app/app.js
+++ app/app.js
@@ -20,27 +20,6 @@ angular.module('modalTest', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.sv
       var beginCount = Math.round(this.myDate / 1000 / 60 / 60 / 24 / 365.25);
       var summary = (dayZero - beginCount);
 
-      //switch(summary) {
-      //  case summary > 5:
-      //    alert('Пора перезаряжать!');
-      //    break;
-      //  case summary >= 5 && this.extinguisher.type === 'carbon_dioxide':
-      //    alert('Пора перезаряжать и проверить целостность раструба');
-      //    break;
-      //  case summary >= 5 && this.extinguisher.type === 'powder':
-      //    alert('Пора перезаряжать и проверить целостность шланга и манометра');
-      //    break;
-      //  case summary >= 2 && this.extinguisher.weight === true:
-      //    alert('Автомобильный. Пора перезаряжать');
-      //    break;
-      //  case summary >= 1:
-      //    alert('Время освидетельствования');
-      //    break;
-      //  default:
-      //    alert('Всё в порядке');
-      //    break;
-      //}
-
       if (summary >= 2 && this.extinguisher.weight === true) {
         alert('Автомобильный. Пора перезаряжать');
       } else if (summary >= 5 && this.extinguisher.type === 'carbon_dioxide') {
@@ -54,8 +33,7 @@ angular.module('modalTest', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.sv
       } else if (summary < 1) {
         alert('Всё в порядке');
       }
-
-      //                                                                                                                                                          $scope.extinguisher.type = null;
+                                                                                                                                                      $scope.extinguisher.type = null;
     };
 
   });
